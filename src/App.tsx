184482109b511172432dbf88220ffe284e15d4c3
cjgreen3 <colin.green@lumen.com>
2024-08-01 09:49:51 -0600

    working changed with centered yellow text

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="text-4xl text-center text-blue-500">
+    <div className="text-4xl text-center text-primary-500">
       I got it to work
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.yellow,
+      },
+    },
   },
   plugins: [],
-}
-
+};
